06770f12c8b6e17f0520c2da06ab200350ded4b6
IsabellaOHara <isabellaohara@btinternet.com>
2022-09-12 12:29:54 +0100

    adding first bean

---
+++ src/main/java/com/qa/BeansApplication.java
@@ -0,0 +1,26 @@
+package com.qa;
+
+import java.time.LocalTime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class BeansApplication {
+
+	public static void main(String[] args) {
+		ApplicationContext context = SpringApplication.run(BeansApplication.class, args);
+		
+		Object byName = context.getBean("seeTime");
+		LocalTime byType = context.getBean(LocalTime.class);
+		LocalTime byBoth = context.getBean("seeTime", LocalTime.class);
+		
+		System.out.println(byName);
+		System.out.println(byType);
+		System.out.println(byBoth);
+		
+		
+	}
+
+}
